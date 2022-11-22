357d0291ec8fb90e114f3d53cdf94f42b3b2c0cc
anazyazd <anazyazd@gmail.com>
2022-11-22 16:52:33 -0600

    added drag and drop logic to job item

--- client/components/Job.jsx
+++ client/components/Job.jsx
@@ -1,13 +1,61 @@
-import React, { useState } from "react";
+import React, { Fragment, useState, useRef } from "react";
+import { reforwardRef } from "react-chartjs-2/dist/utils";
+import { useDrag, useDrop } from "react-dnd";
 import Select from "react-dropdown-select";
 import { connect } from 'react-redux';
+import ITEM_TYPE from "../data/types";
 
 
-
-const Job = ({ info }) => {
+// ------- in order to move the items, included item, index, moveItem, and status ----- //
+const Job = ({ info, index, moveItem, item, appStatus }) => {
   //destructure info prop
   const { role, company, location, status, contact, referral, salary, note } = info;
 
+  const ref = useRef(null);
+  
+  // ----- drop action ------ //
+  const [, drop] = useDrop({
+    accept: ITEM_TYPE,
+    hover(item, monitor){
+      if (!ref.current){
+        return;
+      }
+      const dragIndex = item.index;
+      const hoverIndex = index;
+
+      if (dragIndex === hoverIndex){
+        return;
+      }
+      const hoveredRect = ref.current.getBoundingClientRect();
+      const hoverMiddleY = (hoveredRect.bottom - hoveredRect.top) / 2;
+      const mousePosition = monitor.getClientOffset();
+      const hoverClientY = mousePosition.y - hoveredRect.top;
+
+      if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) return;
+      if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) return;
+
+      moveItem(dragIndex, hoverIndex);
+      item.index = hoverIndex;
+    }
+  });
+
+  // ------ drag action ------ //
+  const [{ isDragging }, drag] = useDrag({
+    item: {type: ITEM_TYPE, ...item, index },
+    collect: monitor => ({
+      isDragging: monitor.isDragging()
+    })
+  });
+
+  // ------ if you want to include a popup window modal ------ //
+
+  const [show, setShow] = useState(false);
+  const onOpen = () => setShow(true);
+  const onClose = () => setShow(false);
+
+  drag(drop(ref));
+
+  // ------- OLD CODE ----- //
 
   const changeStatus = event => {
     // console.log(event.target.value);
